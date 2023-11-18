e4bc9a9d494bd4bff85944fed28f594343527f67
jacinta mwangi <jacintawmwangi21@gmail.com>
2023-11-18 22:17:05 +0300

    added logo

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,10 +2,10 @@
 export default function LandingPage(){
 return(
 <div className="flex h-screen">
-    <div className="w-2/3 min-h-screen">
-
+    <div className="w-2/3 h-screen bg-[url('/landing.jpg')] bg-cover bg-center">
+        
     </div>
-    <div className="w-1/3 min-h-screen bg-gradient-to-r from-indigo-500 to-sky-300">world</div>
+    <div className="w-1/3 h-screen bg-gradient-to-r from-brown-300 to-brown-500">world</div>
 </div>
 )
 }
