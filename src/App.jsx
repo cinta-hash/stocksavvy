7ee11b9378d746fced4377618200c4c61ac110a6
jacinta mwangi <jacintawmwangi21@gmail.com>
2023-11-23 16:03:34 +0300

    added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./Login";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from './About';
+import NoMatchRoute from './NoMatchRoute';
 
 export default function App(){
   return(
@@ -12,6 +13,7 @@ export default function App(){
       <Route path='/' element={<Home />}></Route>
       <Route path='about' element={<About />}></Route>
       <Route path='login' element={<Login />}></Route>
+      <Route path='*' element={<NoMatchRoute />}></Route>
     </Routes>
     </>
   )
---
+++ src/NoMatchRoute.jsx
@@ -0,0 +1,5 @@
+export default function NoMatchRoute(){
+    return(
+        <h1 className="text-6xl text-center font-bold mt-16">Page Not Found</h1>
+    )
+}
