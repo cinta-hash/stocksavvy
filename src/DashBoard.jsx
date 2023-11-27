e4351f413c921f7158f8549cc2707bce093d8399
jacinta mwangi <jacintawmwangi21@gmail.com>
2023-11-27 22:18:57 +0300

    mobile responsiveness

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -2,11 +2,11 @@ import {Link, Outlet} from 'react-router-dom'
 export default function DashBoard(){
     return(
         <div className="h-screen bg-[url('/landing.jpg')] bg-cover bg-center font-raleway">
-            <nav className='flex justify-center space-x-16 p-4 text-2xl font-bold '>
-                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer' to="products">Products</Link>
-                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer' to="sales-orders">Sales Orders</Link>
-                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer' to="purchase-orders">Purchase Orders</Link>
-                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer' to="production-scheduling">Production Scheduling</Link>
+            <nav className='flex flex-col md:flex-row justify-center md:space-x-16 p-4 text-2xl font-bold'>
+                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer mb-4 md:mb-0' to="products">Products </Link>
+                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer mb-4 md:mb-0' to="sales-orders"> Sales Orders</Link>
+                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer mb-4 md:mb-0' to="purchase-orders"> Purchase Orders</Link>
+                <Link className='border-b-2 border-violet-500 active:bg-violet-400 active:duration-300 active:font-bold border-opacity-0 hover:border-opacity-100 hover:text-violet-700 duration-200 cursor-pointer mb-4 md:mb-0' to="production-scheduling"> Production Scheduling</Link>
             </nav>
             <Outlet />
         </div>
